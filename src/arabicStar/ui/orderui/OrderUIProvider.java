cc93fffdd5bc02aa1ce6e8251525d56fdf8264cb
WanZ <598127577@qq.com>
2016-10-21 00:47:33 +0800

    按照组长的写法先写一个Order和Promotion的UI

---
+++ src/arabicStar/ui/orderui/OrderUIProvider.java
@@ -0,0 +1,31 @@
+package arabicStar.ui.orderui;
+
+import arabicStar.uibase.ContentProvider;
+import javafx.event.Event;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.layout.StackPane;
+
+/**
+ * 参考组长的写法
+ * @author WaterWan
+ *
+ */
+public class OrderUIProvider implements ContentProvider {
+
+	@Override
+	public boolean ready(Event e) {
+		// stub
+		return true;
+	}
+
+	@Override
+	public Scene prepareContent() {
+		// stub
+		StackPane p = new StackPane();
+		Label l = new Label("Done");
+		p.getChildren().add(l);
+		return new Scene(p, 300, 300);
+	}
+
+}
