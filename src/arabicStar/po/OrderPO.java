dffc68c4fc6929b146731518ee8dd0669c72eeec
WanZ <598127577@qq.com>
2016-10-21 00:36:31 +0800

    OrderPO加了get set方法

--- src/arabicStar/po/OrderPO.java
+++ src/arabicStar/po/OrderPO.java
@@ -1,14 +1,13 @@
 package arabicStar.po;
 
+import java.util.Date;
 
 public class OrderPO {
 	private HotelPO hotel;
 	private String number;
 	private RoomPO room;
+	private Date date;
 	
-	
-	
-
 	public HotelPO getHotel() {
 		return hotel;
 	}
@@ -20,9 +19,25 @@ public class OrderPO {
 	public String getNumber() {
 		return number;
 	}
+	
+	public Date getDate() {
+		return date;
+	}
+
+	public void setHotel(HotelPO hotel) {
+		this.hotel = hotel;
+	}
+
+	public void setRoom(RoomPO room) {
+		this.room = room;
+	}
 
 	public void setNumber(String number) {
 		this.number = number;
+	}
+	
+	public void setDate(Date date) {
+		this.date = date;
 	}
 
 }
