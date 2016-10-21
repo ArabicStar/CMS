0e95171ef990bec82aba854995ee07b736999398
ClefZz <tinker19981@hotmail.com>
2016-10-22 00:39:36 +0800

    modified:   src/arabicStar/blService/marketBLService/MarketLogicService.java 	modified:   src/arabicStar/blService/memberBLService/MemberLogicService.java 	modified:   src/arabicStar/blServiceImpl/marketBLServiceImpl/MarketLogicServiceImpl_Stub.java 	modified:   src/arabicStar/blServiceImpl/memberBLServiceImpl/MemberLogicServiceImpl_Stub.java 	modified:   src/arabicStar/clientLauncher/driver/Client.java 	modified:   src/arabicStar/clientLauncher/driver/MarketLogicService_Driver.java 	modified:   src/arabicStar/clientLauncher/driver/MemberLogicService_Driver.java 	new file:   src/arabicStar/vo/CreditChangeVO.java 	modified:   src/arabicStar/vo/PromotionVO.java

--- src/arabicStar/blService/marketBLService/MarketLogicService.java
+++ src/arabicStar/blService/marketBLService/MarketLogicService.java
@@ -1,21 +1,33 @@
 package arabicStar.blService.marketBLService;
 
-import arabicStar.util.market.Market;
-import arabicStar.vo.ResultMessage;
+import arabicStar.vo.CreditChangeVO;
+import arabicStar.vo.OrderVO;
+import arabicStar.vo.PromotionVO;
 
 /**
  * market的逻辑接口
+ * 
  * @author ParanoiaSun
  *
  */
 public interface MarketLogicService {
 
-	/**
-	 * 传入修改后的网站营销人员信息进行信息更新
-	 * @param market 修改后的market对象
-	 * @return 修改的提示信息
-	 * TODO:返回值类型为ResultMessage
-	 */
-	public ResultMessage updateInfo(Market market);
+	public boolean makeNewPromotion(PromotionVO newPromotion);
+
+	public PromotionVO[] getCurrentPromotion();
+
+	public boolean deletePromotion(String id);
+
+	public boolean updatePromotion(PromotionVO changedPromotion);
+
+	public PromotionVO getPromotion(String id);
+
+	public OrderVO[] getAbnormalOrders();
+
+	public OrderVO findAbnornalOrder(String orderID);
+
+	public boolean rescueOrder(String orderID);
+
+	public CreditChangeVO chargeCredit(int money);
 
 }
